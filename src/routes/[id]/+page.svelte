ee406f7a76a6add26f3ea624c6b75eb21f15c2f8
Jesse Kramer <jessekramer15@gmail.com>
2024-09-04 14:52:22 +0200

    JK - Added page per person

---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+    
+    // Check if the data has been received and is an array
+    console.log("Received data in +page.svelte:", data);
+</script>
+
+<!-- Only render if we have people in the data -->
+{#if data.person}
+        <h1>{data.person.name}</h1>
+        <p>{data.person.bio}</p>
+{:else}
+    <!-- This will show if no people are available -->
+    <p>No data available</p>
+{/if}
