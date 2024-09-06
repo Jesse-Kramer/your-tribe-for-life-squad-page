a20eff4c1dd7bf9da052852133e3f1a3edeee2b2
Jesse Kramer <jessekramer15@gmail.com>
2024-09-06 14:43:19 +0200

    JK - Added voorbeelden van components and added routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,24 @@
     // Check if the data has been received and is an array
     console.log("Received data in +page.svelte:", data);
 
-    import Logo from '../components/logo.svelte';
+    import Logo from '../components/logo.svelte'; // Voorbeeld logo import
+    import Header from '../components/header.svelte'; // Voorbeeld header import
 </script>
+<Header/> // Voorbeeld header import
 
-<Logo/>
+<Logo/> // Voorbeeld logo import
+
+<hr>
+
+<a href="/1">Ga naar tribe 1</a><br>
+<a href="/2">Ga naar tribe 2</a><br>
+
+<a href="/person/8">Ga naar een persoon (id:8)</a><br>
+
+<a href="/squad/1">Ga naar tribe 1</a><br>
+
+<hr>
+
 <!-- Only render if we have people in the data -->
 {#if data.people}
     {#each data.people as person}
