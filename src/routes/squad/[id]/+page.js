3d81dc1b9b2089effd1fef82705a942ac8e2e819
OvenMagnetron <jessekramer15@gmail.com>
2024-09-06 21:12:43 +0200

    JK - Added list of persons by squad_id

--- src/routes/squad/[id]/+page.js
+++ src/routes/squad/[id]/+page.js
@@ -1,13 +1,27 @@
 /** @type {import('./$types').PageLoad} */
 import { error } from '@sveltejs/kit';
 import getDirectusInstance from '$lib/directus';
-import { readItem } from '@directus/sdk';
+import { readItem, readItems } from '@directus/sdk';
+
 export async function load({ fetch, params }) {
 	const directus = getDirectusInstance(fetch);
 
 	try {
+		// Fetch the squad details by params.id
+		const squad = await directus.request(readItem('squad', params.id));
+		
+		// Fetch people from the same squad using squad_id filter
+		const people = await directus.request(readItems('person', {
+			filter: {
+				squad_id: {
+					_eq: params.id
+				}
+			}
+		}));
+
 		return {
-			squad: await directus.request(readItem('squad', params.id)),
+			squad,
+			people,
 		};
 	} catch (err) {
 		throw error(404, 'Page not found');
--- src/routes/squad/[id]/+page.svelte
+++ src/routes/squad/[id]/+page.svelte
@@ -11,3 +11,14 @@
     <!-- This will show if no people are available -->
     <p>No data available</p>
 {/if}
+
+<!-- Only render if we have people in the data -->
+{#if data.people}
+    {#each data.people as person}
+        <h1>{person.name}</h1>
+        <p>{person.bio}</p>
+    {/each}
+{:else}
+    <!-- This will show if no people are available -->
+    <p>No data available</p>
+{/if}
