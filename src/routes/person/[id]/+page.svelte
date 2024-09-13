91a55c154e388d4f3017feebad2860843688d172
OvenMagnetron <jessekramer15@gmail.com>
2024-09-13 11:20:44 +0200

    JK - Fixed mugshots
    Could be coded better. Will do it soon

--- src/routes/person/[id]/+page.svelte
+++ src/routes/person/[id]/+page.svelte
@@ -7,6 +7,22 @@
 {#if data.person.name}
         <h1>{data.person.name}</h1>
         <p>{data.person.bio}</p>
+        {#if data.person.name && data.person.surname}
+            {#if data.person.prefix}
+                <img alt="Foto van {data.person.name}" 
+                    src="/mugshots/{data.person.name}_{data.person.prefix}_{data.person.surname}.png"
+                    on:error={(event) => event.target.src = data.person.avatar && data.person.avatar.includes("https://") ? data.person.avatar : '/placeholder.png'}>
+            {:else}
+                <img alt="Foto van {data.person.name}" 
+                    src="/mugshots/{data.person.name}_{data.person.surname}.png"
+                    on:error={(event) => event.target.src = data.person.avatar && data.person.avatar.includes("https://") ? data.person.avatar : '/placeholder.png'}>
+            {/if}
+        {:else if data.person.avatar && data.person.avatar.includes("https://")}
+            <img alt="Foto van {data.person.name}" src="{data.person.avatar}">
+        {:else}
+            <img alt="Placeholder" src="/placeholder.png">
+        {/if}
+    
 {:else}
     <!-- This will show if no people are available -->
     <p>No data available</p>
