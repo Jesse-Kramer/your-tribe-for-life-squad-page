109ca5c5ff0061a05bd9b4d29a92b5d6b5d3b607
Jesse Kramer <jessekramer15@gmail.com>
2024-09-13 14:28:10 +0200

    JK - Moved it into +page.js

--- src/routes/person/[id]/+page.svelte
+++ src/routes/person/[id]/+page.svelte
@@ -5,24 +5,13 @@
 
 <!-- Only render if we have people in the data -->
 {#if data.person.name}
-        <h1>{data.person.name}</h1>
-        <p>{data.person.bio}</p>
-        {#if data.person.name && data.person.surname}
-                {#if data.person.prefix}
-                <img alt="Foto van {data.person.name}" 
-                    src={`/mugshots/${data.person.name.replace(/\s+/g, '-')}_${data.person.prefix.replace(/\s+/g, '-')}_${data.person.surname.replace(/\s+/g, '-')}.png`}
-                    on:error={(event) => event.target.src = data.person.avatar && data.person.avatar.includes("https://") ? data.person.avatar : '/placeholder.png'}>
-            {:else}
-                <img alt="Foto van {data.person.name}" 
-                    src={`/mugshots/${data.person.name.replace(/\s+/g, '-')}_${data.person.surname.replace(/\s+/g, '-')}.png`}
-                    on:error={(event) => event.target.src = data.person.avatar && data.person.avatar.includes("https://") ? data.person.avatar : '/placeholder.png'}>
-            {/if}
-        {:else if data.person.avatar && data.person.avatar.includes("https://")}
-            <img alt="Foto van {data.person.name}" src="{data.person.avatar}">
-        {:else}
-            <img alt="Placeholder" src="/placeholder.png">
-        {/if}
-    
+    <h1>{data.person.name}</h1>
+    <p>{data.person.bio}</p>
+    <img 
+        alt="Foto van {data.person.name}" 
+        src="{data.person.imageUrl}"
+        on:error={(event) => event.target.src = data.person.placeholderImage}
+    />
 {:else}
     <!-- This will show if no people are available -->
     <p>No data available</p>
