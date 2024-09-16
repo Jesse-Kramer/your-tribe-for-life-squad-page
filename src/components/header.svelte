c94a3b4c619cccfc4754ea0aec79f83209544be5
Mossati <xmossati9786@gmail.com>
2024-09-16 15:44:46 +0200

    header

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,113 +1,40 @@
 <script>
-    import { Hamburger } from 'svelte-hamburgers';
-    import { fly } from 'svelte/transition';
-    import { slide } from 'svelte/transition';
-	import { quintOut } from 'svelte/easing';
-    import { onMount } from 'svelte';
+onMount(() => {
+    if (data.squads && data.people) {
+        // Maakt een nieuwe class aan en zet deze in de variabele squadIdsWithPersons
+        const squadIdsWithPersons = new Set(data.people.map(person => person.squad_id));
 
-    let open = false;
-    let windowWidth = 0;
-
-    const handleResize = () => {
-        windowWidth = window.innerWidth;
-    };
-
-    onMount(() => {
-        handleResize();
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    });
+        // Filtert de squads die in de squadIdsWithPersons zitten en zet deze in de variabele squadsWithPersons
+        squadsWithPersons = data.squads.filter(squad => squadIdsWithPersons.has(squad.id));
+    }
+});
 </script>
 
-<header>
-    <div>
-        <h1>FDND</h1>
-        <Hamburger class="hamburger" bind:open />
-    </div>
-
-    <nav>
-        {#if open || windowWidth >= 768}
-        <ul transition:fly={{ y: 200, duration: 1000 }}>
-            {#each ['Class 2C', 'Class 2D', 'Contact Us'] as link, i}
-            <li transition:fly={{ y: -15, delay: 50 * i }}>
-                <a href="#">{link}</a>
+<section class="navigation">
+<h1>FDND</h1>
+<ul class="classes-list">
+    {#if squadsWithPersons.length > 0}
+        {#each squadsWithPersons as squad}
+            <li>
+                <a href="/squad/{squad.id}">{squad.name}</a>
             </li>
-            {/each}
-        </ul>
-        {/if}
-    </nav>
-</header>
+        {/each}
+    {:else}
+        <!-- Als hij leeg is -->
+        <p>??</p>
+    {/if}
+</ul>
+<ul class="year-list">
+    {#if data.tribes}
+        {#each data.tribes as tribe}
+            <li>
+                <a href="/{tribe.id}">{tribe.name}</a>
+            </li>
+        {/each}
+    {:else}
+        <!-- Als hij leeg is -->
+        <p>??</p>
+    {/if}
 
-<style>
-    header {
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        position: fixed;
-        top: 0;
-        left: 0;
-        z-index: 2;
-        margin-bottom: 2rem;
-        background-color: var(--bg-color);
-    }
-
-    div {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        width: inherit;
-        padding: 0 3.5rem;
-    }
-
-    nav {
-        background-color: var(--bg-color);
-        width: 100vw;
-    }
-
-    ul {
-        list-style-type: "";
-        margin: 0;
-        padding: 0;
-        height: 100vh;
-    }
-
-    a {
-        display: block;
-        color: var(--text-color);
-        text-decoration: none;
-        padding: 1rem 3.5rem;
-        text-wrap: nowrap;
-    }
-
-    a:hover {
-        text-decoration: underline;
-    }
-
-    @media screen and (min-width: 768px) {
-        header {
-            flex-direction: row;
-            padding: 0 3.5rem;
-        }
-
-        div {
-            padding: 0;
-        }
-
-        ul {
-            height: 100%;
-            display: flex;
-            flex-direction: row;
-            justify-content: end;
-            align-items: center;
-        }
-
-        :global(.hamburger) {
-            position: absolute;
-            top: -10rem;
-        }
-    }
-</style>
+</ul>
+</section>
