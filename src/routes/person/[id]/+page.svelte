d1d188e384d32f8c79b090ac744277a70fae58e1
OvenMagnetron <jessekramer15@gmail.com>
2024-09-16 15:42:25 +0200

    JK - Fixed images

--- src/routes/person/[id]/+page.svelte
+++ src/routes/person/[id]/+page.svelte
@@ -5,13 +5,14 @@
 
 <!-- Only render if we have people in the data -->
 {#if data.person.name}
-    <h1>{data.person.name}</h1>
-    <p>{data.person.bio}</p>
-    <img 
-        alt="Foto van {data.person.name}" 
-        src="{data.person.imageUrl}"
-        on:error={(event) => event.target.src = data.person.placeholderImage}
-    />
+    <picture>
+        <img alt="" src={`/mugshots/${data.person.id}.png`} onerror="this.src= '/placeholder-error-white.png'">
+    </picture>
+
+    <article>
+        <h3>{data.person.name || "Naam?"}</h3>
+        <p>{data.person.bio || "Ik heb geen bio!"}</p>
+    </article>
 {:else}
     <!-- This will show if no people are available -->
     <p>No data available</p>
