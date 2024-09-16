41929a718e36d93ba4dc726338194f40e9f9a869
Patrickkhr <patrickhrdjk@gmail.com>
2024-09-16 20:32:38 +0200

    PH - Styling person page

--- src/routes/person/[id]/+page.svelte
+++ src/routes/person/[id]/+page.svelte
@@ -3,6 +3,19 @@
     export let data;
 </script>
 
+<section class="navigation">
+    <a href="/">&lt; Terug</a>
+    <ul class="classes-list">
+        <li>1F</li>
+        <li>2C</li>
+        <li>2D</li>
+    </ul>
+    <ul class="year-list">
+        <li>Jaar 1</li>
+        <li>Jaar 2</li>
+    </ul>
+</section>
+
 <section class="hero">
     <h2>Persoons pagina</h2>
 </section>
@@ -10,14 +23,25 @@
 <!-- Only render if we have people in the data -->
 <section class="list">
     {#if data.person.name}
-        <picture>
-            <img alt="" src={`/mugshots/${data.person.id}.png`} onerror="this.src= '/placeholder-error-white.png'">
-        </picture>
-
-        <article>
+    <article class="container">
+        <div class="Name">
             <h3>{data.person.name || "Naam?"}</h3>
+        </div>
+        <div class="description">
+            <h4>Front-end Developer</h4>
+        </div>
+        <div class="bio">
+            <h5>Over {data.person.name},</h5>
             <p>{data.person.bio || "Ik heb geen bio!"}</p>
-        </article>
+            <button>test</button>
+        </div>
+        <div class="image">
+            <picture>
+                <img alt="" src={`/mugshots/${data.person.id}.png`} onerror="this.src= '/placeholder-error-white.png'">
+            </picture>
+        </div>
+    </article>
+    
     {:else}
         <!-- This will show if no people are available -->
         <p>No data available</p>
@@ -25,26 +49,77 @@
 </section>
 
 <style>
-    section.list {
-        column-gap: 1rem;
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-        padding: 0 3.5rem;
-        row-gap: .5rem;
+    ul {
+            list-style-type: none;
+            padding: 0;
+        }
+    .navigation {
+        align-items: center;
+        display: flex;
+        justify-content: space-between;
+        padding: 1rem .5rem;
     }
 
+    .navigation ul {
+        display: flex;
+        gap: 1rem;
+    }
+    .navigation a {
+        color: black;
+        text-decoration: none;
+        font-size: 16px;
+    }
+    .navigation a {
+        text-decoration: underline;
+    }
+    .container {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-rows: repeat(6, auto);
+        gap: 0px 8px;
+    }
+    .Name {grid-area: 2 / 1 / 3 / 5;}
+    .description {grid-area: 3 / 1 / 4 / 5;}
+    .bio {
+        grid-area: 5 / 1 / 6 / 5;
+         margin-top: 1em;
+    }
+    .image {grid-area: 4 / 1 / 5 / 5;}
+    section.list {
+        column-gap: 1rem;
+        padding: .5rem;
+        row-gap: .4rem;
+        margin-bottom: 3em;
+    }
     h2 {
         color: #8C8C8C;
         margin: 0;
-        padding: 1rem 3.5rem;
+        padding: 1rem .5rem;
         font-size: 0.8rem;
         text-transform: uppercase;
     }
-
     h3 {
-        margin-bottom: 0;
+        font-size: 52px;
+        margin: 0;
+        float: inline-end;
     }
 
+    h4 {
+        font-size: 32px;
+        font-weight: 100;
+        margin: 0;
+        float: inline-end;
+    }
+    
+    h5 {
+        font-size: 32px;
+        margin: 0;
+    }
+    
+    p {
+        font-size: 20px;
+        font-weight: 100;
+    }
     picture {
         align-items: center;
         display: flex;
@@ -54,7 +129,6 @@
         position: relative;
         width: 100%;
     }
-
     picture::after {
         background: #8C8C8C80;
         border-radius: 8px;
@@ -77,21 +151,56 @@
         width: 100%;
         z-index: 1;
     }
-
-    @media (max-width: 800px) {
+    @media (min-width: 800px) {
+        .navigation {
+            padding: 1rem 3.5rem;
+        }
+        .navigation a {
+            font-size: 24px;
+        }
         section.list {
-            padding: .5rem;
-            row-gap: .4rem;
-            grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+            padding: 0 3.5rem;
         }
-
+        h2 {
+            padding: 1rem 3.5rem;
+        }
+        h3 {
+        font-size: 72px;
+    }
+        h4 {
+            font-size: 52px;
+        }
         picture {
-            height: 200px;
+            height: 400px;
+        }
+        picture::after {
+            height: calc(100% - 50%);
+        }
+        picture img {
+        height: auto;
+        object-fit: none;
+        }
+    }
+    @media (min-width: 1024px) {
+        .container {
+            grid-template-columns: repeat(12, 1fr); 
+            grid-template-rows: repeat(8, minmax(0px, 100px)); 
+            gap: 0px 16px;                          
         }
 
-
+        .Name { grid-area: 2 / 6 / 3 / 11; }
+        .description {
+            grid-area: 3 / 1 / 4 / 11;
+        }
+        .bio {
+            grid-area: 5 / 7 / 6 / 11;
+            margin: 0;
+        }
+        .image {
+            grid-area: 4 / 2 / 7 / 6;
+        }
         picture::after {
-            height: calc(100% - 4em);
+            height: calc(100% - 20%);
         }
     }
 </style>
