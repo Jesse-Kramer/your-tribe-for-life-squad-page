3cae503f7657b1f4cdf2ae65a7c889dfec6370b2
Jesse Kramer <jessekramer15@gmail.com>
2024-09-18 15:22:51 +0200

    JK - Fixed 500 internal error

--- src/routes/person/[id]/+page.js
+++ src/routes/person/[id]/+page.js
@@ -1,16 +1,22 @@
 /** @type {import('./$types').PageLoad} */
 import { error } from '@sveltejs/kit';
 import getDirectusInstance from '$lib/directus';
-import { readItem } from '@directus/sdk';
+import { readItem, readItems } from '@directus/sdk';
 
 export async function load({ fetch, params }) {
     const directus = getDirectusInstance(fetch);
 
     try {
         const person = await directus.request(readItem('person', params.id));
+        const tribes = await directus.request(readItems('tribe'));
+        const squads = await directus.request(readItems('squad', {
+			sort: ['name'],
+		}));
 
         return {
-            person
+            person,
+            tribes,
+            squads
         };
     } catch (err) {
         throw error(404, 'Page not found');
